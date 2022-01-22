1fcfd9f54ceeb41530863f99038015faedbbd889
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-01-22 21:34:36 +0300

    Update queries.js

--- graphql/queries.js
+++ graphql/queries.js
@@ -4,20 +4,28 @@ export function getUsers(){
         avatar_url
         id
         username
+        status
     }`
 }
 
 export function getTasks(limit = 5){
     return `tasks(limit: ${limit}){
             description
-            created_at
-            comments_id
+            created_task_at
             description
             followers_id
-            group_id
-            id
-            isDone
-            user_id
+    }`
+}
+
+export function tasks_By_pk(id){
+    return `tasks_by_pk(id: ${id}) {
+        comments_id
+        created_at
+        description
+        followers_id
+        group_id
+        id
+        isDone
     }`
 }
 
